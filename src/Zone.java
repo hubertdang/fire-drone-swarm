1a8fa83fdfdf86782760caafc31eccf84b973ee1
Aashna-Verma <aashna.verma@outlook.com>
2025-01-29 17:51:19 -0500

    chore(zone): add javadocs

--- src/Zone.java
+++ src/Zone.java
@@ -1,48 +1,95 @@
-import java.util.Objects;
-
+/**
+ * Represents a zone in a 2D space with a unique ID, center position, fire severity,
+ * and required agent amount for firefighting.
+ */
 public class Zone {
     private int id;
     private Position position;
     private FireSeverity severity;
-    private float requiredAgentAmount;
+    private float requiredAgents;
 
-    public Zone(Position position, float requiredAgentAmount) {
-        this.position = position;
-        this.requiredAgentAmount = requiredAgentAmount;
+    /**
+     * Constructs a Zone with a given ID, required agent amount, and boundaries.
+     * The center position is calculated as the midpoint of the given coordinates.
+     *
+     * @param id the unique identifier of the zone
+     * @param requiredAgents the amount of agent required for firefighting
+     * @param startX the starting x-coordinate of the zone
+     * @param endX the ending x-coordinate of the zone
+     * @param startY the starting y-coordinate of the zone
+     * @param endY the ending y-coordinate of the zone
+     */
+    public Zone(int id, float requiredAgents, int startX, int endX, int startY, int endY) {
+        this.id = id;
+        this.requiredAgents = requiredAgents;
+
+        float centerX = (float)(startX + endX) / 2;
+        float centerY = (float)(startY + endY) / 2;
+        this.position = new Position( centerX, centerY);
     }
 
+    /**
+     * Sets the severity level of the fire in this zone.
+     *
+     * @param severity the fire severity level
+     */
     public void setSeverity(FireSeverity severity) {
         this.severity = severity;
     }
 
+    /**
+     * Gets the severity level of the fire in this zone.
+     *
+     * @return the fire severity level
+     */
     public FireSeverity getSeverity() {
         return this.severity;
     }
+    
 
-    public void setPosition(Position position) {
-        this.position = position;
-    }
-
-    public Position getPosition() {
+    /**
+     * Gets the center position of this zone.
+     *
+     * @return the center position of the zone
+     */
+    public Position getCenterPosition() {
         return this.position;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    /**
+     * Gets the unique identifier of this zone.
+     *
+     * @return the ID of the zone
+     */
     public int getId() {
         return this.id;
     }
 
-    public void setRequiredAgentAmount(float amountRequired) {
-        this.requiredAgentAmount = amountRequired;
+    /**
+     * Sets the required amount of agent needed for firefighting in this zone.
+     *
+     * @param requiredAgents the amount of agent required
+     */
+    public void setRequiredAgents(float requiredAgents) {
+        this.requiredAgents = requiredAgents;
     }
 
-    public float getRequiredAgent() {
-        return this.requiredAgentAmount;
+    /**
+     * Gets the required amount of agent needed for firefighting in this zone.
+     *
+     * @return the required agent amount
+     */
+    public float getRequiredAgents() {
+        return this.requiredAgents;
     }
 
+    /**
+     * Checks if this zone is equal to another object. Two zones are equal if they have the same ID,
+     * required agent amount, center position, and severity level.
+     *
+     * @param obj the object to compare
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -50,8 +97,8 @@ public class Zone {
 
         Zone zone = (Zone) obj;
         return id == zone.id &&
-                Float.compare(zone.requiredAgentAmount, requiredAgentAmount) == 0 &&
-                Objects.equals(position, zone.position) &&
+                Float.compare(zone.requiredAgents, requiredAgents) == 0 &&
+                position.equals(zone.position) &&
                 severity == zone.severity;
     }
 }
