4885375511ec5a8c19e12e4800ffbafe09eb52f3
Aashna-Verma <aashna.verma@outlook.com>
2025-01-29 16:59:00 -0500

    feat(zone): add method definitions

--- src/Zone.java
+++ src/Zone.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Zone {
     private int id;
     private Position position;
@@ -5,38 +7,51 @@ public class Zone {
     private float requiredAgentAmount;
 
     public Zone(Position position, float requiredAgentAmount) {
+        this.position = position;
+        this.requiredAgentAmount = requiredAgentAmount;
     }
 
     public void setSeverity(FireSeverity severity) {
+        this.severity = severity;
     }
 
     public FireSeverity getSeverity() {
-        return null;
+        return this.severity;
     }
 
     public void setPosition(Position position) {
+        this.position = position;
     }
 
     public Position getPosition() {
-        return null;
-    }
-
-    public int getId() {
-        return 0;
+        return this.position;
     }
 
     public void setId(int id) {
+        this.id = id;
     }
 
-    public float getRequiredAgent() {
-        return 0;
+    public int getId() {
+        return this.id;
     }
 
     public void setRequiredAgentAmount(float amountRequired) {
+        this.requiredAgentAmount = amountRequired;
+    }
+
+    public float getRequiredAgent() {
+        return this.requiredAgentAmount;
     }
 
     @Override
     public boolean equals(Object obj) {
-        return false;
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Zone zone = (Zone) obj;
+        return id == zone.id &&
+                Float.compare(zone.requiredAgentAmount, requiredAgentAmount) == 0 &&
+                Objects.equals(position, zone.position) &&
+                severity == zone.severity;
     }
 }
