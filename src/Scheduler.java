4df9f3d00d1d340a94b0a1dc0d142c373eeddfae
Manit <manitjawa@gmail.com>
2025-01-29 14:40:25 -0500

    Completed the implementation of the class  - need to complete the function for comparing priority  - need to set up the main entry point of the program

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -3,9 +3,11 @@ public class Scheduler implements Runnable {
     private MissionQueue missionQueue;
 
     public Scheduler() {
+        missionQueue = new MissionQueue();
     }
 
     public void addDrone(Drone drone) {
+        this.drone = drone;
     }
 
     @Override
@@ -13,18 +15,34 @@ public class Scheduler implements Runnable {
     }
 
     public void handleFireReq(Zone zone) {
+        missionQueue.queue(zone);
     }
 
     public void droneStatusUpdated(DroneStatus status) {
+        if (status == DroneStatus.ARRIVED){
+            drone.releaseAgent();
+        }
+        else if (status == DroneStatus.BASE) {
+            //drone.recover();
+        }
+        else {}
     }
 
     public void zoneSeverityUpdated(Zone zone) {
+        if (zone.getSeverity() == FireSeverity.NO_FIRE){
+            drone.stopAgent();
+            drone.fly(new Position(0 ,0));
+        }
     }
 
     public void dispatch(Drone drone, Zone zone) {
+        if (drone.getZoneToService() == null || comparePriority(drone.getZoneToService(), zone)){
+            drone.setZoneToService(missionQueue.pop());
+            drone.fly(zone.getPosition());
+        }
     }
 
     public boolean comparePriority(Zone zone1, Zone zone2) {
-        return false;
+       return false;
     }
 }
