f5779c674c28ee8b47ed5e7bd44ff73232a5fd95
Amilesh Nanthakumaran <62160154+AmileshN@users.noreply.github.com>
2025-02-02 12:43:00 -0500

    Added SimEvent test and renamed class to correct name (#45)
    * Added SimEvent test and renamed class to correct name
    
    * Added test for SimEvent

---
+++ src/SimEventTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimEventTest {
+    private SimEvent simEvent;
+
+    @BeforeEach
+    public void setUp() {
+        simEvent = new SimEvent(1000, 1, "FIRE_DETECTED", "High");
+    }
+
+    @Test
+    public void testGetTime() {
+        assertEquals(1000, simEvent.getTime());
+    }
+
+    @Test
+    public void testGetZoneId() {
+        assertEquals(1, simEvent.getZoneId());
+    }
+
+    @Test
+    public void testGetEventType() {
+        assertEquals("FIRE_DETECTED", simEvent.getEventType());
+    }
+
+    @Test
+    public void testGetSeverity() {
+        assertEquals("High", simEvent.getSeverity());
+    }
+}
