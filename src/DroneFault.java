a5d914956b40287358ec66057dd85dd0ca5d706b
Manit <manitjawa@gmail.com>
2025-03-28 11:59:58 -0400

    docs(DroneSubsystem, DroneFault): Added javadocs and reformatted code

--- src/DroneFault.java
+++ src/DroneFault.java
@@ -1,9 +1,21 @@
+/**
+ * DroneFault class represents faults that will be injected in the drone.
+ */
 public class DroneFault {
     private final int droneId;
     private final int faultCode;
     private final long faultTime;
     private final Faults faultType;
 
+    /**
+     * Constructor for Drone Faults.
+     * Constructs a new fault.
+     *
+     * @param droneId   id of drone that is associated with the fault
+     * @param faultCode the code of the fault
+     * @param faultTime then time when fault is supposed to occur
+     * @param faultType the type of fault
+     */
     public DroneFault(int droneId, int faultCode, long faultTime, Faults faultType) {
         this.droneId = droneId;
         this.faultCode = faultCode;
@@ -11,22 +23,50 @@ public class DroneFault {
         this.faultType = faultType;
     }
 
+    /**
+     * Returns the droneId
+     *
+     * @return droneId
+     */
     public int getDroneId() {
         return droneId;
     }
 
+    /**
+     * Returns the fault code
+     *
+     * @return faultCode
+     */
     public int getFaultCode() {
         return faultCode;
     }
 
+    /**
+     * Returns the time when fault occurs
+     *
+     * @return faultTime
+     */
     public long getFaultTime() {
         return faultTime;
     }
 
+    /**
+     * Returns the type of fault i.e.
+     * Nozzle Jam, or
+     * Drone Stuck Mid-Flight, or
+     * Packet Loss/Corrupted Message
+     *
+     * @return faultType
+     */
     public Faults getFaultType() {
         return faultType;
     }
 
+    /**
+     * Returns a String representation of the fault
+     *
+     * @return String representation of the fault
+     */
     @Override
     public String toString() {
         return "Fault[DroneId = " + droneId + ", Fault Code = " + faultCode + ", Fault Time = " +
--- src/DroneSubsystem.java
+++ src/DroneSubsystem.java
@@ -9,20 +9,27 @@ import java.util.Comparator;
 
 public class DroneSubsystem {
 
+    // a list of drone faults that will be injected
     private static ArrayList<DroneFault> droneFaults;
 
+    /**
+     * Reads a file containing drone faults and adds them to arraylist droneFaults.
+     *
+     * @param filePath The path to the file containing drone faults
+     * @throws IOException
+     */
     public static void readFaultsFile(String filePath) throws IOException {
         String currentLine = null;
         boolean isHeader = true;
 
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             while ((currentLine = br.readLine()) != null) {
-                if (isHeader) {
+                if (isHeader) { // skip the header line of the file
                     isHeader = false;
                     continue;
                 }
 
-                String[] data = currentLine.split(",");
+                String[] data = currentLine.split(","); // get data from each line
                 int droneId = Integer.parseInt(data[0].trim());
                 int faultCode = Integer.parseInt(data[1].trim());
 
@@ -30,8 +37,8 @@ public class DroneSubsystem {
                 int hours = Integer.parseInt(time[0]);
                 int minutes = Integer.parseInt(time[1]);
                 int seconds = Integer.parseInt(time[2]);
-                long faultTime = hours * 3600L + minutes * 60L + seconds; // actual time
-                //long faultTime = minutes * 60L + seconds; // sped up time
+                // convert fault time to seconds since midnight
+                long faultTime = hours * 3600L + minutes * 60L + seconds;
 
                 Faults faultType = Faults.valueOf(String.valueOf(data[3].trim()));
                 droneFaults.add(new DroneFault(droneId, faultCode, faultTime, faultType));
@@ -41,9 +48,14 @@ public class DroneSubsystem {
             e.printStackTrace();
         }
 
-        sortDroneFaults(droneFaults);
+        sortDroneFaults(droneFaults); // sort droneFaults arraylist in ascending order of faultTime
     }
 
+    /**
+     * Method for sorting arraylist droneFaults in ascending order of faultTime.
+     *
+     * @param droneFaults the arraylist containing drone faults
+     */
     private static void sortDroneFaults(ArrayList<DroneFault> droneFaults) {
         droneFaults.sort((fault1, fault2) -> {
             long time1 = fault1.getFaultTime();
@@ -52,28 +64,16 @@ public class DroneSubsystem {
         });
     }
 
-    public ArrayList<DroneFault> getDroneFaults() {
-        return droneFaults;
-    }
-
-    public void setDroneFaults(ArrayList<DroneFault> droneFaults) {
-        this.droneFaults = droneFaults;
-    }
-
     public static void main(String[] args) throws IOException {
         /* TODO: Find a way to not hardcode port numbers (maybe with static field and offsetting */
         Drone drone = new Drone(1, 5001);
         DroneController droneController = new DroneController(drone, 6001);
         Drone drone1 = new Drone(2, 5002);
         DroneController droneController1 = new DroneController(drone1, 6002);
-        droneFaults = new ArrayList<>();
+        droneFaults = new ArrayList<>(); // initialize the arraylist for storing drone faults
 
+        // read the file containing drone faults and store all faults in the arraylist droneFaults
         readFaultsFile("./sample_input_files/faults.csv");
-        sortDroneFaults(droneFaults);
-
-        /*for (DroneFault fault : droneFaults) {
-          System.out.println(fault.toString());
-        }*/
 
         Thread droneThread = new Thread(drone, "🛫D");
         Thread droneControllerThread = new Thread(droneController, "🛫DC");
@@ -86,21 +86,23 @@ public class DroneSubsystem {
         droneThread1.start();
         droneControllerThread1.start();
 
+        // a loop that injects faults when fault time is reached
         while (true) {
-
-            //System.out.println(getCurrentTime());
             for (DroneFault f : droneFaults) {
-                //System.out.println(f.getFaultTime());
                 if (f.getFaultTime() == getCurrentTime()) {
                     System.out.println(f.toString());
                     DroneFault currentFault = droneFaults.remove(droneFaults.indexOf(f));
+
                     if (f.getFaultType().equals(Faults.NJ)) {
+                        // actions to be taken when nozzle jams
                         /*TODO: Actions for Nozzle Jam Fault*/
                     }
                     else if (f.getFaultType().equals(Faults.DSMF)) {
+                        // actions to be taken when drone is stuck mid-flight
                         /*TODO: Actions for Drone Stuck Mid-Flight Fault*/
                     }
                     else if (f.getFaultType().equals(Faults.PLCM)) {
+                        // actions to be taken when there is a packet loss or corrupted message
                         /*TODO: Actions for Packet Loss or Corrupted Message Fault*/
                     }
                     else {
@@ -119,5 +121,23 @@ public class DroneSubsystem {
     private static long getCurrentTime() {
         LocalTime now = LocalTime.now(); // Get current local time
         return now.toSecondOfDay();
+    }
+
+    /**
+     * Returns the arraylist droneFaults.
+     *
+     * @return droneFaults
+     */
+    public ArrayList<DroneFault> getDroneFaults() {
+        return droneFaults;
+    }
+
+    /**
+     * Sets the arraylist droneFaults.
+     *
+     * @param droneFaults empty arraylist that will be used to store drone faults
+     */
+    public void setDroneFaults(ArrayList<DroneFault> droneFaults) {
+        this.droneFaults = droneFaults;
     }
 }
